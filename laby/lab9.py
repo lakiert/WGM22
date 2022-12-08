aec81f370e6678260da5f65789c706cf319ab77b
lakiert <73653530+lakiert@users.noreply.github.com>
2022-12-08 08:47:33 +0100

    Update lab9.py

--- laby/lab9.py
+++ laby/lab9.py
@@ -4,6 +4,8 @@ from PIL import ImageChops
 import matplotlib.pyplot as plt
 
 
+# zad 1
+
 obraz1 = Image.open("obraz1.png")
 # obraz1.show()
 
@@ -17,4 +19,28 @@ obraz3.paste(obraz1, (0, 0), a)
 # obraz3.save("obraz3.png")
 
 difference = ImageChops.difference(obraz2, obraz3)
-difference.show()
+# difference.show()
+
+
+# zad 2
+
+tryb1 = obraz3.convert('CMYK')
+tryb2 = obraz3.convert('YCbCr')
+tryb3 = obraz3.convert('HSV')
+
+
+plt.figure(figsize=(16, 8))
+plt.subplot(1, 3, 1)
+plt.title('RGB')
+plt.imshow(tryb1)
+plt.axis('off')
+plt.subplot(1, 3, 2)
+plt.title('YCbCr')
+plt.imshow(tryb2)
+plt.axis('off')
+plt.subplot(1, 3, 3)
+plt.title('HSV')
+plt.imshow(tryb3)
+plt.axis('off')
+plt.savefig('fig1.png')
+plt.show()
